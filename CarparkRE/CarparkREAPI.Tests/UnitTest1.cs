bde0260d62f2056ef7f5ade7d7088406bcc59674
andrewsweaney <andrewsweaney@gmail.com>
2021-04-16 12:15:17 +1000

    Fixed Package reference and completed testing API

--- CarparkRE/CarparkREAPI.Tests/UnitTest1.cs
+++ CarparkRE/CarparkREAPI.Tests/UnitTest1.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//using CarparkRE.Controllers;
-//using CarparkRE_Lib.Models;
+using CarparkRE.Controllers;
+using CarparkRE_Lib.Models;
 using Newtonsoft.Json;
 
 namespace CarparkREAPI.Tests
@@ -10,7 +10,7 @@ namespace CarparkREAPI.Tests
     [TestClass]
     public class UnitTest1
     {
-        /*
+        
         [TestMethod]
         public void APITestStandardRate()
         {
@@ -80,19 +80,74 @@ namespace CarparkREAPI.Tests
         [TestMethod]
         public void APITestNightRate()
         {
+            // Requirements:
+            //         Name: Night Rate
+            //         Type: Flat Rate
+            //         Total Price: $6.50
+            //         Entry Condition: Enter between 6:00 PM to Midnight
+            //         Exit Condition: Exit before 8 AM
+            //                          Assumption: Exiting before 8 AM is intended to be 8 AM the next day
 
+            var engine = new RateEngineController();
+
+            DateTime dtTest = new DateTime(2021, 4, 15);
+            CPRateRQ rq = new CPRateRQ();
+
+            // Night Rate - Normal weekday test
+            rq.EntryDT = dtTest.AddHours(18).AddMinutes(30);         // Thursday 18:30 (6:30PM)
+            rq.ExitDT = dtTest.AddDays(1).AddHours(7);               // Friday 07:00 AM
+            var rs = JsonConvert.DeserializeObject<CPRateRS>(engine.GetRate(rq));
+            Assert.IsTrue(rs.RateName == "Night Rate");
+            Assert.IsTrue(rs.TotalPrice == 6.5m);
+
+            // Night Rate - Friday night test
+            rq.EntryDT = dtTest.AddDays(1).AddHours(18).AddMinutes(30);     // Friday 18:30 (6:30PM)
+            rq.ExitDT = dtTest.AddDays(2).AddHours(7);                      // Saturday 07:00 AM
+            rs = JsonConvert.DeserializeObject<CPRateRS>(engine.GetRate(rq));
+            Assert.IsTrue(rs.RateName == "Night Rate");
+            Assert.IsTrue(rs.TotalPrice == 6.5m);
         }
 
         [TestMethod]
         public void APITestWeekendRate()
         {
+            // Requirements:
+            //         Name: Weekend Rate
+            //         Type: Flat Rate
+            //         Total Price: $10.00
+            //         Entry Condition: Enter anytime past midnight on Friday
+            //         Exit Condition: Exit any time before midnight on Sunday
 
+            var engine = new RateEngineController();
+
+            DateTime dtTest = new DateTime(2021, 4, 17);
+            CPRateRQ rq = new CPRateRQ();
+
+            // Weekend Rate
+            rq.EntryDT = dtTest.AddMinutes(12);                             // Saturday 00:12 AM
+            rq.ExitDT = dtTest.AddDays(1).AddHours(12).AddMinutes(47);      // Sunday 12:47 PM
+            var rs = JsonConvert.DeserializeObject<CPRateRS>(engine.GetRate(rq));
+            Assert.IsTrue(rs.RateName == "Weekend Rate");
+            Assert.IsTrue(rs.TotalPrice == 10);
         }
 
         [TestMethod]
         public void APITestCheapestRate()
         {
+            // Requirements:
+            //         The customer should get the cheapest deal based on the rules which apply to the time period
 
-        }*/
+            var engine = new RateEngineController();
+
+            DateTime dtTest = new DateTime(2021, 4, 15);
+            CPRateRQ rq = new CPRateRQ();
+
+            // Crossover Case: Qualifies for Standard Rate - 7 Hours 47 Mins ($20) or Early Bird Rate ($13)
+            rq.EntryDT = dtTest.AddHours(9);                    // Thursday 9:00 AM
+            rq.ExitDT = dtTest.AddHours(16).AddMinutes(47);     // Thursday 16:47 (4:47 PM)
+            var rs = JsonConvert.DeserializeObject<CPRateRS>(engine.GetRate(rq));
+            Assert.IsTrue(rs.RateName == "Early Bird");
+            Assert.IsTrue(rs.TotalPrice == 13);
+        }
     }
 }
